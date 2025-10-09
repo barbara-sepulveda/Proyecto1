86ca698d0384c07f9dc279572ea8eb6f7223ca1b
barbara-sepulveda <118142237+barbara-sepulveda@users.noreply.github.com>
2025-10-09 10:31:36 -0300

    Update views.py

--- incidencias/views.py
+++ incidencias/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Categoria
+from .forms import CategoriaForm
 
-# Create your views here.
+def categoria_list(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'incidencias/categoria_list.html', {'categorias': categorias})
+
+def categoria_create(request):
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categoria_list')
+    else:
+        form = CategoriaForm()
+    return render(request, 'incidencias/categoria_form.html', {'form': form})
+
+def categoria_update(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            form.save()
+            return redirect('categoria_list')
+    else:
+        form = CategoriaForm(instance=categoria)
+    return render(request, 'incidencias/categoria_form.html', {'form': form})
+
+def categoria_delete(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+    if request.method == 'POST':
+        categoria.delete()
+        return redirect('categoria_list')
+    return render(request, 'incidencias/categoria_confirm_delete.html', {'categoria': categoria})
