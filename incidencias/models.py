22355cf942deabb1a4862b8f01137dc0d8fdf8bc
barbara-sepulveda <118142237+barbara-sepulveda@users.noreply.github.com>
2025-10-09 10:30:32 -0300

    Update models.py

--- incidencias/models.py
+++ incidencias/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
 # Create your models here.
